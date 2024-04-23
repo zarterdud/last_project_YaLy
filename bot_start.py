5def14558a08ca3406e568b2ee073b6f207f32c4
Andrey Saderdinov <andreysaderdinov@gmail.com>
2024-04-23 16:14:34 +0300

    add start

--- bot_start.py
+++ bot_start.py
@@ -2,6 +2,7 @@ from tg_bot_main import MyBot
 from db_api.database import DataBase
 from config import TOKEN, db_name
 from telegram.ext import ApplicationBuilder
+from main import app
 
 
 bot = ApplicationBuilder().token(TOKEN).build()
@@ -9,3 +10,4 @@ db = DataBase(db_name)
 
 my_bot = MyBot(application=bot, database=db)
 my_bot.run()
+app.run(debug=True)
