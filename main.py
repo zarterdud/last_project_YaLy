136e8a48b9ed6e5f4d27122b066d542d8c059312
Andrey Saderdinov <andreysaderdinov@gmail.com>
2024-04-23 11:43:03 +0300

    update

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)  # Создание экземпляра приложения Flask
 
+
 def parse_content(url):
-    data = {  # Создание словаря для хранения данных
-        "title": "",
-        "content": []
-    }
+    data = {"title": "", "content": []}  # Создание словаря для хранения данных
 
     try:  # Обработка исключений для запросов
         response = requests.get(url)
         if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
+            soup = BeautifulSoup(response.text, "html.parser")
 
             # Извлечение заголовка
             data["title"] = soup.title.string if soup.title else ""
 
             # Извлечение контента
-            for tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']):
+            for tag in soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6", "p"]):
                 content = {"tag": tag.name, "text": tag.text.strip()}
                 data["content"].append(content)
 
@@ -28,16 +26,22 @@ def parse_content(url):
 
     return data
 
-@app.route('/', methods=['GET', 'POST'])  # Маршрут для главной страницы
+
+@app.route("/", methods=["GET", "POST"])  # Маршрут для главной страницы
 def home():
-    if request.method == 'POST':  # Проверка метода запроса
-        url = request.form['url']  # Получение URL из формы
+    if request.method == "POST":  # Проверка метода запроса
+        url = request.form["url"]  # Получение URL из формы
         if url:  # Проверка наличия URL
             parsed_data = parse_content(url)  # Парсинг контента
-            return render_template('index.html', data=parsed_data, url=url)  # Возврат шаблона с данными
+            return render_template(
+                "index.html", data=parsed_data, url=url
+            )  # Возврат шаблона с данными
         else:
-            return render_template('index.html', error="Введите URL.", url="")  # Возврат шаблона с ошибкой
-    return render_template('index.html', url="")  # Возврат пустого шаблона
+            return render_template(
+                "index.html", error="Введите URL.", url=""
+            )  # Возврат шаблона с ошибкой
+    return render_template("index.html", url="")  # Возврат пустого шаблона
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
